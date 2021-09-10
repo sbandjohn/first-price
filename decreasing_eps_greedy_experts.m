baf3910a3b858574e12ca40ce2987be4cc84cd65
tao-l <869306176@qq.com>
2021-09-09 21:20:44 -0400

    add two equilibrium

---
+++ decreasing_eps_greedy_experts.m
@@ -0,0 +1,46 @@
+function res = decreasing_eps_greedy_experts(P1, P2, T, func1, func2)
+M1 = size(P1, 1); M2 = size(P2, 2); 
+
+sum_reward_1 = zeros(M1, 1);
+sum_reward_2 = zeros(M2, 1);
+
+bid_seq_1 = zeros(1, T); 
+bid_seq_2 = zeros(1, T); 
+
+strategy_seq_1 = zeros(M1, T);
+strategy_seq_2 = zeros(M2, T);
+
+for t = 1:T
+    eps_1 = func1(t); 
+    [~, argmax_1] = max(sum_reward_1); 
+    tmp_1 = zeros(M1, 1); 
+    tmp_1(argmax_1) = 1;
+    prop_1 = eps_1 * ones(M1, 1) / M1  +  (1-eps_1) * tmp_1; 
+    
+    eps_2 = func2(t); 
+    [~, argmax_2] = max(sum_reward_2); 
+    tmp_2 = zeros(M2, 1); 
+    tmp_2(argmax_2) = 1;
+    prop_2 = eps_2 * ones(M2, 1) / M2  +  (1-eps_2) * tmp_2; 
+    
+    b_1 = randsample(M1, 1, true, prop_1);
+    b_2 = randsample(M2, 1, true, prop_2);
+    
+    sum_reward_1 = sum_reward_1 + P1(:, b_2);
+    sum_reward_2 = sum_reward_2 + P2(b_1, :)'; 
+    
+    % reward_1 = R(b_r, b_c);
+    % reward_c = C(b_r, b_c);
+    
+    % count_r(b_r) = count_r(b_r) + 1; 
+    % avg_reward_r(b_r) = ((count_r(b_r)-1)*avg_reward_r(b_r) + reward_r) / count_r(b_r); 
+    % count_c(b_c) = count_c(b_c) + 1;
+    % avg_reward_c(b_c) = ((count_c(b_c)-1)*avg_reward_c(b_c) + reward_c) / count_c(b_c);
+    
+    bid_seq_1(t) = b_1;  bid_seq_2(t) = b_2; 
+    strategy_seq_1(:, t) = prop_1;
+    strategy_seq_2(:, t) = prop_2;
+end
+
+res = {bid_seq_1, bid_seq_2, strategy_seq_1, strategy_seq_2}; 
+end
---
+++ two_equilibriium.m
@@ -0,0 +1,115 @@
+N = 2;
+M = 4;
+v1 = M;
+v2 = M; 
+P1 = zeros(M);
+P2 = zeros(M);
+
+for i = 0:(M-1)
+    P1(i+1, i+1) = (v1-i)/2;  P2(i+1, i+1) = (v2-i)/2;
+    for j = 0:(M-1)
+        if (j < i)
+            P1(i+1, j+1) = v1-i;  P2(i+1, j+1) = 0;
+        else
+            if (j > i)
+               P1(i+1, j+1) = 0;  P2(i+1, j+1) = v2-j;  
+            end
+        end
+    end
+end
+
+
+T = 100;
+
+% res = MWU(R, C, T, eps_r, eps_c, "uniform", "uniform")
+% res = eps_greedy(R, C, T, eps_r, eps_c);
+
+c1 = 1; c2 = 1;
+para_func_1 = @(t) c1 / sqrt(t); 
+para_func_2 = @(t) c2 / sqrt(t); 
+handle = @() decreasing_eps_greedy_experts(P1, P2, T, para_func_1, para_func_2); 
+name = "decreasing \epsilon greedy, experts"; 
+
+
+%%%%%%%%%% BEGIN: show results for once %%%%%%%%%%%%
+res = handle();
+
+bid_seq_1 = res{1}; 
+bid_seq_2 = res{2}; 
+strategy_seq_1 = res{3}; 
+strategy_seq_2 = res{4}; 
+
+frequency_seq = get_frequency_seq(bid_seq_1, M); 
+
+xs = 1:T; 
+
+figure()
+bids = [0 1 2 3]; 
+styles = {"-.", ":", "-", "--"}; 
+for i = bids
+    plot(xs, frequency_seq(i+1, :), styles{i+1}, "LineWidth", 1.5)
+    hold on 
+end
+legend("0", "1", "2", "3")
+title("bid frequency of player 1 -- "+name+" -- v1="+num2str(M)) 
+hold off
+
+figure()
+for i = bids
+    plot(xs, strategy_seq_1(i+1, :), styles{i+1}, "LineWidth", 1.5)
+    hold on
+end
+legend("0", "1", "2", "3")
+title("strategy of player 1 -- "+name+" -- v1="+num2str(M)) 
+hold off
+
+
+%%%%%%%%%% END: show results for once %%%%%%%%%%%%
+
+
+%%%%%%%%%% BEGIN: count the times of the two NE %%%%%%%%%%%%%%
+res = count_which_NE(M, T, 1000, handle)
+%%%%%%%%%% END: count the times of the two NE %%%%%%%%%%%%%%
+
+
+
+
+
+%%%%%%%% helper functions %%%%%%%%%%%%%
+
+function f_seq = get_frequency_seq(a_seq, M)
+T = size(a_seq, 2); assert(size(a_seq, 1) == 1); 
+f_seq = zeros(M, T);
+f_seq(a_seq(1), 1) = 1; 
+for t = 2:T
+    f_seq(:, t) = f_seq(:, t-1); 
+    f_seq(a_seq(t), t) = f_seq(a_seq(t), t-1) + 1; 
+end
+for t = 2:T
+    f_seq(:, t) = f_seq(:, t) ./ t; 
+end
+end
+
+function res = count_which_NE(M, T, times, handle)
+    second = 0;
+    first = 0;
+    other = 0;
+    for i = 1:times
+        res = handle(); 
+        bid_seq = res{1}; 
+        f_seq = get_frequency_seq(bid_seq, M); 
+        f = f_seq(:, T); 
+        if f(M) > 0.9
+            second = second + 1; 
+        else
+            if f(M-1) > + 0.9
+                first = first + 1;
+            else
+                other = other + 1;
+            end
+        end
+    end
+    res = [first, second, other]; 
+end
+
+
